68a49eb20a93c832037818d71d43ffe5ba2607e0
JainamKarania <jainamkarania05@gmail.com>
2025-04-18 15:56:56 +0530

    prisma installation

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-    '/sign-in(.*)',
-    '/sign-up(.*)'
-  ])
-  
-  
-export default clerkMiddleware((auth, req) => {
-    if (!isPublicRoute(req)) {
-      auth().protect()
-    }
-  })
+  '/sign-in(.*)',
+  '/sign-up(.*)'
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+     auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
@@ -19,5 +18,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
-
+}
